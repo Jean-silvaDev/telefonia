970168766cde089b517c2e991b23bab8021be45a
jean-silva365 <jeanfrancisco2018@outlook.com>
2024-11-28 09:02:41 -0300

    Primeiro commit

---
+++ src/main/java/bcc/ifsuldeminas/sistemaDeTelefonia/model/entities/telefonia/comercial/Plano.java
@@ -0,0 +1,44 @@
+package bcc.ifsuldeminas.sistemaDeTelefonia.model.entities.telefonia.comercial;
+
+//Nome: Jean Francisco Da Silva
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Plano {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    private float valorPorMinuto;
+
+    public Plano() {}
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getValorPorMinuto() {
+        return valorPorMinuto;
+    }
+
+    public void setValorPorMinuto(float valorPorMinuto) {
+        this.valorPorMinuto = valorPorMinuto;
+    }
+}
---
+++ src/main/java/bcc/ifsuldeminas/sistemaDeTelefonia/model/entities/telefonia/pessoal/ClientePessoaJuridica.java
@@ -0,0 +1,22 @@
+package bcc.ifsuldeminas.sistemaDeTelefonia.model.entities.telefonia.pessoal;
+
+//Nome: Jean Francisco Da Silva
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(value = "ClientePessoaJuridica")
+public class ClientePessoaJuridica extends Cliente{
+    private String cnpj;
+
+    public ClientePessoaJuridica() {}
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+}
